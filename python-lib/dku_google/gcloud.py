814d42bc3f8c5db997f6695755e932bd3a8cf41f
Harizo Rajaona <harizo.rajaona@dataiku.com>
2019-10-15 18:06:08 +0200

    Fix indentation in _get_gcloud_info

--- python-lib/dku_google/gcloud.py
+++ python-lib/dku_google/gcloud.py
@@ -9,9 +9,9 @@ def _get_gcloud_info():
         try:
             gcloud_info_str = subprocess.check_output(["gcloud", "info", "--format", "json"])
             GCLOUD_INFO = json.loads(gcloud_info_str)
-            return GCLOUD_INFO
         except:
             raise ValueError("gcloud CLI not found, check if Google Cloud SDK is properly installed and configured.")
+    return GCLOUD_INFO
     
 def get_sdk_root():
     sdk_root = _safe_get_value(_get_gcloud_info(), ["installation", "sdk_root"], None)
